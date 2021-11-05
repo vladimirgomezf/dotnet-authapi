c78c47ea608b9be060ddf0ddec4db9159c4dabe0
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2021-11-05 15:14:20 -0400

    ci: Add the model and context from db with EF

---
+++ dotnet-authapi/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+#nullable disable
+
+namespace dotnet_authapi.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        [JsonIgnore] public string Password { get; set; }
+    }
+}
