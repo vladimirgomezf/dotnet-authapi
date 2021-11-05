0e43734bfd07f4a03e4e6808bd1c4db2d46b5d7f
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2021-11-05 15:15:41 -0400

    feat: create the interface and the implementation od the user repository

---
+++ dotnet-authapi/Models/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_authapi.Models
+{
+    public interface IUserRepository
+    {
+        User Create(User user);
+        User GetByEmail(string email);
+        User GetById(int id);
+    }
+}
---
+++ dotnet-authapi/Models/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_authapi.Models
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly authapiContext _context;
+
+        public UserRepository (authapiContext context)
+        {
+            _context = context;
+        }
+
+        public User Create(User user)
+        {
+            _context.Users.Add(user);
+            user.Id = _context.SaveChanges();
+
+            return user;
+        }
+
+        public User GetByEmail (string email)
+        {
+            return _context.Users.FirstOrDefault(u => u.Email == email);
+        }
+
+        public User GetByiD(int id)
+        {
+            return _context.Users.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
