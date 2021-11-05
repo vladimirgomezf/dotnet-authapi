e255bf1d3df847597ff36292fad93a208db69820
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2021-11-05 15:16:08 -0400

    feat: Add the JwtService to create the security Token

---
+++ dotnet-authapi/Helpers/JwtService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_authapi.Helpers
+{
+    public class JwtService
+    {
+        public string secureKey = "this is a very secure key";
+
+        public string Generate(int id)
+        {
+            var symetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secureKey));
+            var credentials = new SigningCredentials(symetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+            var header = new JwtHeader(credentials);
+
+            var playload = new JwtPayload(id.ToString(), null, null, null, DateTime.Today.AddDays(1));
+            var securityToken = new JwtSecurityToken(header, playload);
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+
+        public JwtSecurityToken Verify(string jwt)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secureKey);
+            tokenHandler.ValidateToken(jwt, new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = false,
+                ValidateAudience = false
+            }, out SecurityToken validatedToken);
+
+            return (JwtSecurityToken)validatedToken;
+        }
+    }
+}
